4dbd1d32d26a07fe7b44066864c6410beef45700
asharulawal <asharulawal99@gmail.com>
2024-08-28 17:23:25 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const themeToggle = document.getElementById('themeToggle');
+    const body = document.body;
+    const triangle = document.querySelector('.triangle');
+  
+    themeToggle.addEventListener('click', () => {
+      body.classList.toggle('night');
+      updateTriangleColor();
+    });
+  
+    function updateTriangleColor() {
+      if (body.classList.contains('night')) {
+        triangle.style.borderLeftColor = '#2c3e50'; // night color
+      } else {
+        triangle.style.borderLeftColor = '#87CEEB'; // day color
+      }
+    }
+    updateTriangleColor();
+  });
+
+  window.addEventListener('load', function() {
+    const music = document.getElementById('backgroundMusic');
+    if (music) {
+      music.play(); // Mulai memutar musik ketika halaman dimuat
+    }
+  });
